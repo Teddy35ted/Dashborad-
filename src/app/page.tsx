e884a723c69998583c14e137c02cf38255b64c89
Ted <tedkouevi708@gmail.com>
2025-03-24 22:32:14 +0100

    page.tsx change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Sidebar from '../components/sidebar/sidebar';
+//import Sidebar from '../components/sidebar/sidebar';
 import Profile from '../components/profile/profile';
 
 export default function Home() {
   return (
     <div className="flex min-h-screen bg-gray-50">
-      <Sidebar />
       <main className="flex-1">
         <Profile />
       </main>
